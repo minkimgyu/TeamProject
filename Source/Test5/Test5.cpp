ef94ba50fef2ba339faf765fc0533851a68af72a
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2023-04-10 20:35:59 +0900

    update1

---
+++ Source/Test5/Test5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Test5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test5, "Test5" );
---
+++ Source/Test5/Test5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST5_API ATest5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
